208589410aa5ec3cb7ca9c849b0a11188dfaefeb
Chris <30739736+Hoxin@users.noreply.github.com>
2018-05-04 21:44:56 -0700

    Add files via upload

---
+++ Crossbow_shooter_game/CharacterCreator.py
@@ -0,0 +1,6 @@
+
+class CharacterCreator():
+    def __init__(self, name, image, playerSpeed):
+        self.name = name
+        self.image = image
+        self.playerSpeed = playerSpeed
--- Crossbow_shooter_game/CrossbowCreator.py
+++ Crossbow_shooter_game/CrossbowCreator.py
@@ -1,14 +1,5 @@
 
-from turtle import *
-import pygame
-
-class Armory():
-    def __init__(self, weaponName, arrow_image, attackPower, arrowRange):
-        self.weaponName = weaponName
-        self.arrow_image = pygame.image.load("resources/image/bullet.png")
-        self.attackpower = attackPower
-        self.screen = self.arrowTurtle.getscreen()
-        self.arrowRange = arrowRange
-    def arrowRange_Set():
-        ArrowRange = print(self.arrowRange)
-        return ArrowRange
+class CrossbowAttributes():
+    def __init__(self, arrowSpeed, damage):
+        self.arrowSpeed = arrowSpeed
+        self.damage = damage
